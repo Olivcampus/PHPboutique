7da0f968e667f78d8e9c9cfcc1321513644a1ebc
Olivier HIMBLOT <olivier.himblot@le-campus-numerique.fr>
2023-05-25 13:52:55 +0200

    etape 2.2

--- pages/item.php
+++ pages/item.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php 
+include 'template/header.php';
+?>
 <body>
     
 <?php 
@@ -15,10 +10,9 @@
     print '<img src = "'.$product1photo.'" alt = "photo teeshirt"/>';
     print "$product1Price ";
     print  " $idProduct1";
-
-    
-
 ?>
 
 </body>
-</html>
+<?php 
+include 'template/footer.php';
+?>
---
+++ pages/template/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <?php
+        $logoEntreprise = "/assets/images/logo_entreprise.png" ;
+            print '<img src = "'.$logoEntreprise.'" alt = "logo Entreprise"/>';
+    ?>
+</footer>
+</html>

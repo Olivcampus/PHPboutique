438e09b49f8a0ac12c34af34cf5eccb29e7373a4
Olivier HIMBLOT <olivier.himblot@le-campus-numerique.fr>
2023-05-25 11:57:49 +0200

    test php 3

--- testCode.php
+++ testCode.php
@@ -7,18 +7,16 @@
     <title>Document</title>
 </head>
 <body>
-
-
-
-
 <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+
 <?php 
+// $ pour dire que c'est une variable (int, float ...)//
 $userAge = 17;
 $userAge = 23;
 $userAge = 55;
-$fullname = "Mathieu Nebra";
-$email = 'mathieu.nebra@exemple.com';
-$variable = "Mon \"nom\" est Mathieu";
+$fullname = "Olivier HIMBLOT";
+$email = 'olivier.himblot@le-camppus-numerique.fr';
+$variable = "Mon \"nom\" est Mathieu"; // \" où \' pour afficher les guillemets sur un site //
 $variable = 'Je m\'appelle Mathieu';
 $isAuthor = true;
 $isAdministrator = false;
@@ -26,11 +24,14 @@ echo 'Bonjour ' . $fullname . ' et bienvenue sur le site !'; // méthode d'affic
 $number = 10;
 $result = ($number + 5) * $number; // $result prend la valeur 150 //
 $isEnabled = true;
-if ($isEnabled == true) {
+if ($isEnabled == true) { //écrire un boolean//
     echo "Vous êtes autorisé(e) à accéder au site ✅";
 }
-else {
+elseif ($isEnabled == false){ // peut s'écrire aussi (! $isEnabled) //
     echo "Accès refusé ❌ ";
+}
+else {
+    echo "j'ai pas tout compris";
 }
 ?>
 
